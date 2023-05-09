fe3754860d920325944a1d981943e646c44f269c
Mike Kistler <mikekistler@microsoft.com>
2023-05-08 19:39:57 -0700

    Updated dotnet study

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -8,32 +8,24 @@ using Azure.AI.AnomalyDetector;
 string _apiKey = Environment.GetEnvironmentVariable("ANOMALY_DETECTOR_APIKEY");
 string _endpoint = Environment.GetEnvironmentVariable("ANOMALY_DETECTOR_ENDPOINT");
 
-#region TASK 1
-/*
- * Create an Anomaly Detector client object that authenticates with an API key.
- * The endpoint and API key to use are stored in the _apiKey and _endpoint variables.
- */
-// CODE FOR TASK 1 GOES HERE
-#endregion
-
-//Task2();
-//var model = Task3();
-//var modelId = Task4();
-// int? numAnomalies = Task5(modelId);
+Task1();
+//var model = Task2();
+//var modelId = Task3();
+// int? numAnomalies = Task4(modelId);
 // Console.WriteLine($"Number of anomalies detected: {numAnomalies}");
-//Task6(modelId);
+//Task5(modelId);
 
-#region TASK 2
+#region TASK 1
 /*
  * Print the number of multivariate models in the service.
  */
-void Task2()
+void Task1()
 {
-    // CODE FOR TASK 2 GOES HERE
+    // CODE FOR TASK 1 GOES HERE
 }
 #endregion
 
-#region TASK 3
+#region TASK 2
 /*
  * Create and train a multivariate anomaly detector model.
  * The training data is located at https://multivariateuserstudy.blob.core.windows.net/anomalydetectordata/training_data.csv
@@ -44,26 +36,24 @@ void Task2()
  * Return the model object when it's ready for use.
  * If the training failed, return null.
  */
-AnomalyDetectionModel Task3()
+AnomalyDetectionModel Task2()
 {
-    // CODE FOR TASK 3 GOES HERE
-    return null!;
+    // CODE FOR TASK 2 GOES HERE
 }
 #endregion
 
-#region TASK 4
+#region TASK 3
 /*
  * Print the number of multivariate models in the service that are ready to use.
  * Return the model ID of the model that you created in Task 3.
  */
-string Task4()
+string Task3()
 {
-    // CODE FOR TASK 4 GOES HERE
-    return null!;
+    // CODE FOR TASK 3 GOES HERE
 }
 #endregion
 
-#region TASK 5
+#region TASK 4
 /*
  * Perform a bulk analysis of multivariate sensor data using the model you created in Task 3.
  * The data is located at https://multivariateuserstudy.blob.core.windows.net/anomalydetectordata/sensors.csv
@@ -72,14 +62,14 @@ string Task4()
  * The data contains data points for the period 2021-01-02T14:20:00Z to 2021-01-03T01:59:00Z.
  * Return the number of anomalies detected.
  */
-int? Task5(string modelId)
+int? Task4(string modelId)
 {
-    // CODE FOR TASK 5 GOES HERE
+    // CODE FOR TASK 4 GOES HERE
     return null;
 }
 #endregion
 
-#region TASK 6
+#region TASK 5
 /*
  * Perform a bulk analysis of multivariate sensor data using the model you created in Task 3.
  * The data is located at https://multivariateuserstudy.blob.core.windows.net/anomalydetectordata/sensors2.csv
@@ -89,8 +79,8 @@ int? Task5(string modelId)
  * There's a problem with the data that will cause the analysis to fail. You should add error handling
  * logic to your function to catch the error and return an English description of the error.
  */
-void Task6(string modelId)
+void Task5(string modelId)
 {
-    // CODE FOR TASK 6 GOES HERE
+    // CODE FOR TASK 5 GOES HERE
 }
 #endregion
